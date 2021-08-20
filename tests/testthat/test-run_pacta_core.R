0ce67b551c288bee147da06eb945269e45a628bd
Mauro Lepore <maurolepore@gmail.com>
2021-08-20 13:10:54 -0600

    WIP test fixture

--- tests/testthat/test-run_pacta_core.R
+++
@@ -1,8 +0,0 @@
-test_that("create the expected output structure", {
-  dir <- withr::local_dir(tempdir())
-  dir <- local_pacta(dir)
-  run_pacta(fs::path(dir, ".env"))
-
-  has_results <- fs::dir_exists(fs::path(dir, "output", "working_dir", "40_Results"))
-  expect_true(has_results)
-})
