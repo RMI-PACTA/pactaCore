f8c0cfb0647b1a590d85cb5c3f8fece9d92b4596
Mauro Lepore <maurolepore@gmail.com>
2021-08-29 19:59:34 -0700

    Prune

--- R/local_env.R
+++ R/local_env.R
@@ -1,13 +1,14 @@
-#' Help create an ephemeral environment file
+#' Help create an environment file
 #'
-#' See https://testthat.r-lib.org/articles/test-fixtures.html#local-helpers.
+#' * `create_env()` creates an environment file.
+#' * `local_env()` creates a temporary environment file -- available only in the
+#' local context where `local_env()` is called.
+#'
+#' @seealso https://testthat.r-lib.org/articles/test-fixtures.html#local-helpers.
 #'
 #' @param path String. Path to the environment file you want to create.
 #' @param ... Passed on to `create_env()`.
-#' @param env Must be passed on to `withr::defer()`.
-#'
-#' @export
-#' @keywords internal
+#' @param envir Must be passed on to `withr::defer()`.
 #'
 #' @examples
 #' # The local context is usually a call to test_that()
@@ -35,15 +36,13 @@
 #' )
 #' file_exists(env)
 #' readLines(env)
+#' @noRd
 local_env <- function(path = path_temp(".env"), ..., envir = parent.frame()) {
   create_env(path, ...)
   withr::defer(file_delete(path), envir = envir)
   invisible(path)
 }
 
-#' @rdname local_env
-#' @export
-#' @keywords inernal
 create_env <- function(path = path_temp(".env"),
                        output = path_temp("output"),
                        input = path_temp("input"),
@@ -59,23 +58,4 @@ create_env <- function(path = path_temp(".env"),
   writeLines(sprintf("%s=%s", envvars, dirs), con = path)
 
   invisible(path)
-}
-
-#' Create a pacta environment file from pacta variables set in .Renviron
-#'
-#' @param path String. Path to the environment file you want to create.
-#'
-#' @export
-#' @keywords internal
-#'
-#' @examples
-#' env <- create_env_from_renviron()
-#' readLines(env)
-create_env_from_renviron <- function(path = path_temp(".env")) {
-  create_env(
-    path,
-    output = Sys.getenv("PACTA_OUTPUT"),
-    input = Sys.getenv("PACTA_INPUT"),
-    data = Sys.getenv("PACTA_DATA")
-  )
 }
