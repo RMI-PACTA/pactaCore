3185d50712d95615f3d7cdd53e5775e171b102e6
Mauro Lepore <maurolepore@gmail.com>
2021-09-08 15:55:34 -0700

    New run_pacta_impl() allows quick tests

--- R/run_pacta.R
+++ R/run_pacta.R
@@ -26,6 +26,13 @@
 #'   })
 #' }
 run_pacta <- function(env = ".env") {
+  run_pacta_impl(env)
+  invisible(env)
+}
+
+# `code` allows injecting test code
+run_pacta_impl <- function(env = ".env",
+                           code = expression(system(docker_run))) {
   input <- path_env("PACTA_INPUT", env)
   output <- path_env("PACTA_OUTPUT", env)
   abort_if_dir_exists(results_path(fs::path_dir(output)))
@@ -50,7 +57,8 @@ run_pacta <- function(env = ".env") {
     "docker run --rm -v %s:/input -v %s:/output -v %s:/pacta-data:ro %s %s",
     input, output, data, image_tag, command_arg
   )
-  system(docker_run)
+
+  eval(code)
 
   invisible(env)
 }
